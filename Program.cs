f2c6a9a7771f3fd44e18afa39d13b3d6f784c3cb
keshavprakash98 <keshavprakash98@gmail.com>
2021-10-20 21:09:44 +0530

    My .Net Program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation program on Master Branch");
+        }
+    }
+}
