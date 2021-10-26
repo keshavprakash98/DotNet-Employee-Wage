bbf8725dfffb00e5c4493ad22523f021fd0e453d
keshavprakash98 <keshavprakash98@gmail.com>
2021-10-26 17:54:06 +0530

    EMPYWAGE

---
+++ UC5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day3
+{
+    class UC5
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAY = 2;
+        static void Main(string[] args)
+        {
+            int empHrs = 0, int empwage = 0, totalEmpWage = 0;
+            for (int day = 0; day < NUM_OF_WORKING_DAY; day++)
+            {
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empwage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp  Wage :" + empwage);
+            }
+        }   Console.WriteLine("Total Emp Wage:" + totalEmpWage);
+    }
+}
+
+
